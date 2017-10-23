f68f9dde69a2b031e0364fd50e8b3c2c3cfddfa5
= <nabcogroup@gmail.com>
2017-10-23 11:47:11 +0300

    fixing mege

--- rootapp/resources/assets/js/components/fixed-asset/FixedAssetRegisterDialog.vue
+++ rootapp/resources/assets/js/components/fixed-asset/FixedAssetRegisterDialog.vue
@@ -1,6 +1,39 @@
 <template>
   <v-dialog dialog-title="Fixed Asset Entry" modal-id="fixedAssetEntry" v-model="toggle" @dismiss="save">
-
+    <div class="form-group row">
+        <label class="col-md-3 col-form-label">Date:</label>
+        <div class="col-md-9">
+          <dt-picker ></dt-picker>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-md-3 col-form-label">Decription:</label>
+        <div class="col-md-9">
+          <input class="form-control" v-model="lookups.description">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-md-3 col-form-label">Property:</label>
+        <div class="col-md-9">
+          <select class="form-control" v-model="lookups.property">
+              <option v-for="look in lookups.property" v-bind:value="look.id">{{ look.property }}</option>
+          </select>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-md-3 col-form-label">Fixed Asset Type:</label>
+        <div class="col-md-9">
+          <select class="form-control" v-model="lookups.fixed_asset_type">
+              <option v-for="look in lookups.fixed_asset_type" v-bind:value="look.id">{{ look.fixed_asset_type }}</option>
+          </select>
+        </div>
+    </div>
+    <div class="form-group row">
+        <label class="col-md-3 col-form-label">Cost:</label>
+        <div class="col-md-9">
+          <input class="form-control" v-model="lookups.cost">
+        </div>
+    </div>
   </v-dialog>
 </template>
 
@@ -16,6 +49,22 @@ import { EventBus } from "../../eventbus";
 export default {
     name: 'FixedAssetRegisterDialog',
     mixins: [toggleModal],
+<<<<<<< HEAD
+=======
+
+    mounted() {
+      this.$store.dispatch('fixedAsset/create');
+    },
+    computed: {
+      ...mapGetters("fixedAsset",{
+        'lookups': 'lookups',
+      }),
+      ...mapState("fixedAsset",{
+        data : state => state.data
+      })
+    },
+
+>>>>>>> Register form added
     data() {
         return {
             validations: new ErrorValidations(),

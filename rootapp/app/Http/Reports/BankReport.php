3b2ff05c6c2dbbbdf762a16caf64f89e0e63613c
= <nabcogroup@gmail.com>
2017-10-23 17:20:37 +0300

    doing deposit per villa

--- rootapp/app/Http/Reports/BankReport.php
+++ rootapp/app/Http/Reports/BankReport.php
@@ -12,13 +12,15 @@ class BankReport extends BaseReport
 {
     use HelperTrait;
 
-
-    
     public function __construct($type,$params)
     {
         if($type == "summary") {
             $this->dataSource = new BankDepositSummary($params);
             $this->templateSource = "bank_report.detail";
+        }
+        else if($type == "per_villa") {
+            $this->dataSource = new VillaBankDeposit($params);
+            $this->templateSource = "bank_report.per_villa";
         }
         else {
             $this->dataSource = new BankDepositDetail($params);

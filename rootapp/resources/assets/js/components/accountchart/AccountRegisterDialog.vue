5b0b2bcab684e97d157891bcc93c7cc28a3c9d01
Bryan Mesias <sevensand29@gmail.com>
2017-10-31 11:44:43 +0300

    AccountRegisterDialog.vue Form created

---
+++ rootapp/resources/assets/js/components/accountchart/AccountRegisterDialog.vue
@@ -0,0 +1,26 @@
+<template>
+<v-dialog dialog-title="Account Chart List" modal-id="accountList" v-model="toggle" @dismiss="save">
+    <div class="form-horizontal">
+      <v-input-wrapper label="Serial Number" label-class="col-md-3 text-right">
+          <input class="form-control" type="text" name="serial_number">
+      </v-input-wrapper>
+
+        <v-input-wrapper label="Property" label-class="col-md-3 text-right">
+            <select class="form-control">
+                <option value="">--ACCOUNT TYPE--</option>
+                <option></option>
+            </select>
+
+        </v-input-wrapper>
+        <v-input-wrapper label="Description" label-class="col-md-3 text-right">
+            <textarea class="form-control"></textarea>
+        </v-input-wrapper>
+
+    </div>
+</v-dialog>
+</template>
+<script>
+  export default {
+    
+  }
+</script>

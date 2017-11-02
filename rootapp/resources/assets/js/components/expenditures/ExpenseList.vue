f3e36bf96d6a1ef824cf58441970cc36c9d540dc
Bryan Mesias <sevensand29@gmail.com>
2017-11-02 11:01:32 +0300

    Expense List Filter Added

--- rootapp/resources/assets/js/components/expenditures/ExpenseList.vue
+++ rootapp/resources/assets/js/components/expenditures/ExpenseList.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-10">
                 <v-search :config="searchObj" @click="onSearch"></v-search>
-            </div>   
+            </div>
             <div class="col-md-2">
                 <button class="btn btn-info btn-block" @click="$store.commit('expenditures/redirectToRegister')"><i
                         class="fa fa-plus" aria-hidden="true"></i> Add Expenses</button>
@@ -12,7 +12,7 @@
         <hr/>
         <div class="row">
             <div class="col-md-12">
-                <v-live-view :grid="gridView" ></v-live-view>
+                <v-live-view :grid="gridView"></v-live-view>
             </div>
         </div>
     </v-panel>
@@ -29,11 +29,15 @@
                 gridView: {
                     columns: [
                         {name: 'payment_date', column: 'Payment Date', class: 'text-center', dtype: 'date'},
-                        {name: 'location', column: 'Villa Location', class: 'text-center'},
-                        {name: 'villa', column: 'Villa No', class: 'text-center'},
-                        {name: 'expense_type', column: 'Expense Type', class: 'text-center'},
+                        {name: 'location', column: 'Villa Location', class: 'text-center', filter: true},
+                        {name: 'villa', column: 'Villa No', class: 'text-center', filter:true},
+                        {name: 'expense_type', column: 'Expense Type', class: 'text-center', filter: true},
                         {name: 'payee', column: 'Paid To', class: 'text-center'},
                         {name: 'amount', column: 'Amount', class: 'text-center'},
+                        {name: '$action', column: ' ', static: true, class: 'text-center', style: 'width:5%'}
+                    ],
+                    actions: [
+                        {key: 'edit', name: 'Edit'}
                     ],
                     source: {
                         url: 'api/expenses',
